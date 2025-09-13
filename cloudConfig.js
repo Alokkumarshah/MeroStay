28a8005a6fe209948903d2ca1f1bb7986a85eca7
Alokkumarshah <alokandmessi@gmail.com>
2025-09-14 04:26:47 +0530

    Finals

--- cloudConfig.js
+++ cloudConfig.js
@@ -11,7 +11,7 @@ const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: 'MeroStay_DEV',
-    allowerdFormats: ["png","jpg","jpeg","HEIF"], // supports promises as well
+    allowedFormats: ["png","jpg","jpeg","heif"], // supports promises as well
    
   },
 });
